5750d0c296c17a3f5b677ea08ec9129eeb77f834
delalik <delalik@mcmaster.ca>
2018-01-27 23:52:24 -0500

    bookmarks to firestore

--- client/src/js/firebase.js
+++ client/src/js/firebase.js
@@ -1,5 +1,6 @@
 import Rebase from 're-base';
 import firebase from 'firebase';
+import 'firebase/firestore'
 
 const config = {
     apiKey: "AIzaSyCQDw7PVaZntzi_TueW0ZiyaJGoqAC0U9g",
@@ -12,5 +13,6 @@ const config = {
 
 const app = firebase.initializeApp(config);
 const base = Rebase.createClass(app.database());
+const firestore = firebase.firestore()
 
-export { base }
+export { base, firestore }
